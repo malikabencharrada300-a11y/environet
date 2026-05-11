7974f8f62ad13aea8066656fd0b7c8c6e5f51fd3
malikabencharrada300-a11y <malikabencharrada300@gmail.com>
2026-05-12 00:18:13 +0100

    Update config .php

--- config .php
+++ config .php
@@ -4,7 +4,7 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-ini_set('log_errors', 1);
+ini_set('log_errors', 1); 
 ini_set('error_log', __DIR__ . '/error.log');
 
 date_default_timezone_set('Europe/Paris');
