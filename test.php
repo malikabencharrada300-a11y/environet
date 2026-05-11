673130b9bda761d711472e3da0a7f0c58b653594
malikabencharrada300-a11y <malikabencharrada300@gmail.com>
2026-05-11 23:17:22 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,25 @@
 <?php
 
-require 'config.php';
+$host = "db.gfwbtyjzpvvbwpixpdap.supabase.co";
+$dbname = "postgres";
+$user = "postgres";
+$password = "ghadaa2004+12+25";
+$port = "5432";
 
-echo "Connexion réussie 🎉";
+try {
+
+    $pdo = new PDO(
+        "pgsql:host=$host;port=$port;dbname=$dbname",
+        $user,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+
+    die("Erreur : " . $e->getMessage());
+
+}
 
 ?>
