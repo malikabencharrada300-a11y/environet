5b86956e8b182cd9fd6efb8566056e5c07dd66d1
malikabencharrada300-a11y <malikabencharrada300@gmail.com>
2026-05-13 20:53:59 +0100

    Update config.php

--- config.php
+++ config.php
@@ -64,6 +64,7 @@ function getDBConnection()
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_PERSISTENT => true,
             ]
         );
 
